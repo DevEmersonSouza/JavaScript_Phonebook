38dd408e40c61d0cdb2d4bb276111f1a012c8fc2
Emerson Souza <souzaemerson003@gmail.com>
2022-10-18 22:19:00 -0300

    adding a modal and some contacts

--- script.js
+++ script.js
@@ -2,23 +2,17 @@
 // POST - subir/adicionar/enviar/criar
 // PUT - atualizar/alterar/
 // DELETE - apagar/deletar/destruir/remover/aniquilar
+$(window).on('load', function () {
+    $('#myModal').modal('show');
+});
 
-window.onload = login();
-
-function login(){
-    let login = document.getElementById("myModal")
-    login.modal({
-        show: true
-      });  
-}
-
-async function addContato(){
+async function addContato() {
     let dados = input_nova_tarefa.value.split(" ")
     let nome = dados[0]
     let idade = dados[1]
-    let chuchu = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3',{
+    let chuchu = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3', {
         method: "POST",
-        headers:{
+        headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
@@ -27,19 +21,19 @@ async function addContato(){
         })
     })
     console.log(chuchu)
-    if(chuchu.ok){
+    if (chuchu.ok) {
         console.log('adicionei')
         atualizarContatos()
     }
 }
 
-async function atualizar(identificador){
+async function atualizar(identificador) {
     let nomeNovo = prompt("nome?")
     let idadeNovo = prompt("idade?")
 
-    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/' + identificador, {
+    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3' + identificador, {
         method: 'PUT',
-        headers:{
+        headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
@@ -47,52 +41,57 @@ async function atualizar(identificador){
             idade: idadeNovo
         })
     });
-    if(res.ok){
+    if (res.ok) {
         alert('Atualizou')
         atualizarContatos()
-    }else{
+    } else {
         alert('Erro ao atualizar')
     }
 
 }
 
 atualizarContatos();
-
-async function atualizarContatos(){
+let nrm = 1
+function cont() {
+    nrm++
+}
+async function atualizarContatos() {
     let resposta = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3')
     let body = await resposta.json()
-    tarefas.innerHTML = "<ul>"
+    contacts.innerHTML = "<div>"
     body.forEach(pessoa => {
-        tarefas.innerHTML +=  ` 
-        <li>${pessoa.nome} - ${pessoa.idade} 
-            <button onclick="deletar(${pessoa.id})">Deletar</button>
-            <button onclick="atualizar(${pessoa.id})">Atualizar</button>
-        </li>`
+        contacts.innerHTML += ` 
+        <input type="text" value="${pessoa.nome}" id="nome` + nrm + `">
+        <input type="text" value="${pessoa.idade}" id="telefone` + nrm + `">
+        <button class="btn btn-outline-primary" onclick="editar()" id="editBtn">editar</button>
+        <button class="btn btn-outline-primary" onclick="concluir()" id=concludeBtn style="display:none">concluir</button>`
+        cont()
     });
-    tarefas.innerHTML +=  "</ul>"
+    contacts.innerHTML += "</div>"
 }
 
-async function deletar(identificador){
-    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/' + identificador, {
+async function deletar(identificador) {
+    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3' + identificador, {
         method: 'DELETE',
     });
-    if(res.ok){
+    if (res.ok) {
         atualizarContatos();
-    } else{
+    } else {
         console.log(res.statusText)
     }
 }
 
-function editar() {
-    teste.disabled = true;
-    editBtn.style.display="none"
-    concludeBtn.style.display="inline"
-    teste.style.color = "#000000"
+function editar(nrm) {
+    nome(nrm).disabled = true;
+    telefone(nrm).disabled = true;
+    editBtn.style.display = "none"
+    concludeBtn.style.display = "inline"
+    console.log("apertei")
 }
 
-function concluir(){
-    teste.disabled = false;
-    teste.style.color = "#ff0000"
-    editBtn.style.display="inline"
-    concludeBtn.style.display="none"
+function concluir(nrm) {
+    nome(nrm).disabled = true;
+    telefone(nrm).disabled = true;
+    editBtn.style.display = "inline"
+    concludeBtn.style.display = "none"
 }
