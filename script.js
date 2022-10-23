ce9a1e279e2f767a743ace2481cffe01fa3e647a
Emerson Souza <souzaemerson003@gmail.com>
2022-10-23 01:02:58 -0300

    added delete, new contact and favourites buttons and functions

--- script.js
+++ script.js
@@ -2,66 +2,117 @@
 // POST - subir/adicionar/enviar/criar
 // PUT - atualizar/alterar/
 // DELETE - apagar/deletar/destruir/remover/aniquilar
+let inputNovoNome = document.getElementById("input_novo_nome")
+let inputNovoNumero = document.getElementById("input_novo_numero")
+let btnAddContato = document.getElementById("btnAddContato")
+let btnMostrarFormContato = document.getElementById("mostrarFormContato")
+let btnCancelarContato = document.getElementById("btnCancelarContato")
 $(window).on('load', function () {
     $('#myModal').modal('show');
 });
 
+function mostrarFormContato() {
+    inputNovoNome.style.display = "inline"
+    inputNovoNumero.style.display = "inline"
+    btnAddContato.style.display = "inline"
+    btnCancelarContato.style.display = "inline"
+    btnMostrarFormContato.style.display = "none"
+}
+function cancelarFormContato() {
+    inputNovoNome.style.display = "none"
+    inputNovoNumero.style.display = "none"
+    btnAddContato.style.display = "none"
+    btnCancelarContato.style.display = "none"
+    btnMostrarFormContato.style.display = "inline"
+}
 async function addContato() {
-    let dados = input_nova_tarefa.value.split(" ")
-    let nome = dados[0]
-    let idade = dados[1]
-    let chuchu = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3', {
+    let nome = input_novo_nome.value
+    let numero = input_novo_numero.value
+
+    if (nome === "" || numero === "") {
+        alert("por favor inserir as informacoes")
+        return
+    }
+    let novoContato = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3', {
         method: "POST",
         headers: {
             'content-type': 'application/json'
         },
         body: JSON.stringify({
             nome: nome,
-            idade: idade
+            idade: numero
         })
     })
-    console.log(chuchu)
-    if (chuchu.ok) {
+    console.log(novoContato)
+    if (novoContato.ok) {
         console.log('adicionei')
         atualizarContatos()
+        input_novo_nome.value = ""
+        input_novo_numero.value = ""
     }
+    cancelarFormContato()
 }
+
 
 
 
 async function atualizarContatos() {
     let resposta = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3')
     let body = await resposta.json()
+    let contador = document.getElementById("numeros")
+    contador.value = "("  + body.length + ")"
     contacts.innerHTML = `<div class="contacts">`
     body.forEach(pessoa => {
         contacts.innerHTML += `<div><input type="text" value="${pessoa.nome}" id="nome${pessoa.id}" disabled="disabled">
         <input type="text" value="${pessoa.idade}" id="telefone${pessoa.id}" disabled="disabled">
-        <button class="btn btn-outline-primary" onclick="editar(${pessoa.id})" id="editBtn">editar</button>
-        <button class="btn btn-outline-primary" onclick="concluir(${pessoa.id})" id="concludeBtn">concluir</button></div>`
-        
+        <button class="btn btn-outline-primary" onclick="editar(${pessoa.id})" id="editBtn${pessoa.id}"><i class="bi bi-pencil-square"></i></button>
+        <button class="btn btn-outline-primary" onclick="concluir(${pessoa.id})" id="concludeBtn${pessoa.id}" style="display: none;"><i class="bi bi-check-circle-fill"></i></button>
+        <button class="btn btn-outline-primary" onclick="deletar(${pessoa.id})" id="deleteBtn"><i class="bi bi-x-circle-fill"></i></button>
+        <button id="favoritos${pessoa.id}" class="btn btn-outline-primary"  onclick="favoritos(${pessoa.id})"><i class="bi bi-bookmark-star-fill"></i></button>
+        <button id="removerfavoritos${pessoa.id}" class="btn btn-outline-primary"  onclick="favoritos(${pessoa.id})" style="display: none;"><i class="bi bi-bookmark-x-fill"></i></button></div>`
+
+
     });
     contacts.innerHTML += `</div>`
 }
 atualizarContatos();
 
+function favoritos(id) {
+    favoriteButton = document.getElementById("favoritos" + id);
+    removerFavoriteButton = document.getElementById("removerfavoritos" + id);
+    removerFavoriteButton.style.display = "inline"
+    removerFavoriteButton.style.color = "black";
+    removerFavoriteButton.style.backgroundColor = "yellow";
+    removerFavoriteButton.style.border = "1px solid black";
+    favoriteButton.style.display = "none"
+}
+
 async function editar(id) {
     let nomeNovo = document.getElementById("nome" + id)
     let telefoneNovo = document.getElementById("telefone" + id)
+    let editBtn = document.getElementById("editBtn" + id)
+    let concludeBtn = document.getElementById("concludeBtn" + id)
+    editBtn.style.display = "none"
+    concludeBtn.style.display = "inline"
     nomeNovo.disabled = false;
     telefoneNovo.disabled = false;
     console.log("editando")
-    
+
 }
 
 async function concluir(id) {
-    
+
     let nomeNovo = document.getElementById("nome" + id).value
     let telefoneNovo = document.getElementById("telefone" + id).value
+    let editBtn = document.getElementById("editBtn" + id)
+    let concludeBtn = document.getElementById("concludeBtn" + id)
+    editBtn.style.display = "inline"
+    concludeBtn.style.display = "none"
     nomeNovo.disabled = true;
     telefoneNovo.disabled = true;
     console.log(nomeNovo)
 
-    let res = await fetch(`https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/`+ id, {
+    let adicionarNumero = await fetch(`https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/` + id, {
         method: 'PUT',
         headers: {
             'content-type': 'application/json'
@@ -71,7 +122,7 @@ async function concluir(id) {
             idade: telefoneNovo
         })
     });
-    if (res.ok) {
+    if (adicionarNumero.ok) {
         alert('Atualizou')
         atualizarContatos()
     } else {
@@ -79,11 +130,12 @@ async function concluir(id) {
     }
 }
 
-async function deletar(identificador) {
-    let res = await fetch('https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3' + identificador, {
+async function deletar(id) {
+    let deletar = await fetch(`https://633867b7937ea77bfdbf9c86.mockapi.io/pessoa3/` + id, {
         method: 'DELETE',
     });
-    if (res.ok) {
+    if (deletar.ok) {
+        console.log("deletei")
         atualizarContatos();
     } else {
         console.log(res.statusText)
